117d743c67299ba1d320b660b3731fd02ac67d52
OndrejZivny <ondrej.zivny.022@pslib>
2024-03-29 21:49:56 +0100

    maďar pomoho :)

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -2,3 +2,13 @@ function otevriHAM(){
     const hammenu = document.querySelector('.hammenu')
     hammenu.style.display = 'flex'
 }
+
+function zavritHAM(){
+    const hammenu = document.querySelector('.hammenu')
+    hammenu.style.display = 'none'
+}
+
+window.addEventListener('resize', function() {
+    const hammenu = document.querySelector('.hammenu')
+    hammenu.style.display = 'none'
+});
