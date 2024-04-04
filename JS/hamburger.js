fb45b35c0c5c65b8298638973f2ddf34d6811934
OndrejZivny <ondrej.zivny.022@pslib>
2024-04-04 11:28:22 +0200

    start payment

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -15,3 +15,14 @@ function zavritHAM(){
 //     const hammenu = document.querySelector('.HAM--menu')
 //     hammenu.style.display = 'none'
 // });
+
+window.addEventListener('resize', function() {
+    const hammenu = document.querySelector('.HAM--menu')
+    if (window.innerWidth > 750) {
+        
+        hammenu.style.display = 'flex'
+    }
+    else{
+        hammenu.style.display = 'none'
+    }
+});
